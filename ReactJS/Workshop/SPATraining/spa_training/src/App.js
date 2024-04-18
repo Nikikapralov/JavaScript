cd4741be63e17ade067c725cdcb492b1f8958632
Nikikapralov <nikikapralov1@gmail.com>
2024-04-18 22:45:50 +0200

    first workshop start

---
+++ ReactJS/Workshop/SPATraining/spa_training/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import { Header } from './components/HTMX/Header.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <Header></Header>
+  )
+}
+
+export default App;
