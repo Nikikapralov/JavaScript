743999515aed2af6bda23da66a8b07371df6d6bf
Nikikapralov <74428148+Nikikapralov@users.noreply.github.com>
2024-05-14 01:46:12 +0200

    Delete ReactJS/Workshop_1/django_server/WorkshopServer_1 directory

--- ReactJS/Workshop_1/django_server/WorkshopServer_1/WorkshopServer_1/cha_encryption/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChaEncryptionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'WorkshopServer_1.cha_encryption'
--- ReactJS/Workshop_1/django_server/WorkshopServer_1/WorkshopServer_1/user_list/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserListConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'WorkshopServer_1.user_list'
