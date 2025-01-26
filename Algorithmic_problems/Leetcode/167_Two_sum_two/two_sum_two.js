73ce2caac3a2177e04cbe191d958c9a36f83640b
Nikikapralov <nikikapralov1@gmail.com>
2025-01-27 00:59:07 +0100

    new leetcode problems

---
+++ Algorithmic_problems/Leetcode/167_Two_sum_two/two_sum_two.js
@@ -0,0 +1,47 @@
+"use strict";
+function two_sum_two(numbers, target) {
+    /*
+        """
+    TWO POINTER SOLUTION
+    Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order,
+    find two numbers such that they add up to a specific target number.
+    Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+    Return the indices of the two numbers, index1 and index2,
+    added by one as an integer array [index1, index2] of length 2.
+    The tests are generated such that there is exactly one solution.
+    You may not use the same element twice.
+    Your solution must use only constant extra space.
+
+    SOLUTION:
+    The array is sorted so we can use a two pointers approach.
+    We put one at the start and one at the end. We then calculate the sum of
+    the values. If its target, we return. If it's bigger, we do -- do the bigger
+    pointer to get a smaller value. If its smaller, we do a ++ to get a bigger value.
+    Eventually we will reach the 2 numbers that do the sum or the pointers will meet resulting
+    in no solution.
+
+    :param nums: An ordered list of integers.
+    :return: A list of integers holding the first 2 numbers that sum up to the target.
+    """
+    */
+    let left_pointer = 0;
+    let right_pointer = numbers.length - 1;
+    let result = [];
+    while (left_pointer != right_pointer) {
+        let left_value = numbers[left_pointer];
+        let right_value = numbers[right_pointer];
+        let summed = left_value + right_value;
+        if (summed == target) {
+            result.push(left_pointer + 1);
+            result.push(right_pointer + 1);
+            break;
+        }
+        else if (summed < target) {
+            ++left_pointer;
+        }
+        else {
+            --right_pointer;
+        }
+    }
+    return result;
+}
